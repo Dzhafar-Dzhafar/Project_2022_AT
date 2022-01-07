0e7b3942f753994ab6efb264948c4c0e31e23555
Dobby <nag_bit_8@mail.ru>
2022-01-07 19:35:40 +0300

    1/7/2022

--- division_by_2.py
+++ division_by_2.py
@@ -1,3 +1,2 @@
 def division(a, b):
     return a / b
-print(division(10, 2))
--- tests/test_division_by_2.py
+++ tests/test_division_by_2.py
@@ -7,3 +7,10 @@ import pytest
 def test_division_true(a, b, result):
     assert division_by_2.division(a, b) == result
 
+#Проверку добавлю деление на '0' и ошибку типа данных
+
+@pytest.mark.parametrize("expected, divider, division", [(ZeroDivisionError, 0, 10),
+                                               (TypeError, "2", 20)])
+def test_zero_division(expected, divider, division):
+    with pytest.raises(ZeroDivisionError):
+        division_by_2.division(division, divider)
