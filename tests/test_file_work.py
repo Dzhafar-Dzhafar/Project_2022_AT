e85c3d9a367da32ea2580fedcf7a2d5a4283b70c
Dobby <nag_bit_8@mail.ru>
2022-01-08 20:04:44 +0300

    1/8/2022

--- tests/test_file_work.py
+++ tests/test_file_work.py
@@ -1,5 +1,6 @@
 from my_funcs.file_work import read_from_file
 
+
 def test_read_from_file():
     test_data = 'one\ntwo\nthree'
-    assert test_data == read_from_file("test_file.txt")
+    assert test_data == read_from_file("tests/test_file.txt")
