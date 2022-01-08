ba1aaf3ca49b664405132f79af997831e3e852d3
Dobby <nag_bit_8@mail.ru>
2022-01-08 15:55:29 +0300

    1/8/2022

--- tests/test_division_by_2.py
+++ tests/test_division_by_2.py
@@ -9,10 +9,13 @@ import pytest
 def test_division_true(a, b, result):
     assert division_by_2.division(a, b) == result
 
+
 @pytest.mark.parametrize("expected, divider, division", [(ZeroDivisionError, 0, 10)])
 def test_zero_division(expected, divider, division):
     with pytest.raises(ZeroDivisionError):
         division_by_2.division(division, divider)
+
+
 @pytest.mark.parametrize("expected, divider, division", [(TypeError, "2", 20)])
 def test_type_error(expected, divider, division):
     with pytest.raises(TypeError):
